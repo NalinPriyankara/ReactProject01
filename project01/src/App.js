7ee2f0f6f6d76d23b1ce89b52b60c6450b806638
NalinPriyankara <112267206+NalinPriyankara@users.noreply.github.com>
2024-04-24 23:09:06 +0530

    component

--- project01/src/App.js
+++ project01/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Part1 from './components/Part1';
 
 function App() {
   return (
     <div className="App">
       <h1>This is my first react project</h1>
+      <Part1/>
+      <Part1/>
     </div>
   );
 }
---
+++ project01/src/components/Part1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Part1() {
+  return (
+    <div>
+      <h1>This is my first react component</h1>
+    </div>
+  )
+}
