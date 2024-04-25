95a7a1448e6dc5adfe090f3517afd14bc1ec8920
NalinPriyankara <112267206+NalinPriyankara@users.noreply.github.com>
2024-04-25 23:02:59 +0530

    router2

--- project01/src/App.js
+++ project01/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div>
        <Switch>
 
-          <Route path="/">
+          <Route exact path="/">
             <Home/>
           </Route>
 
--- project01/src/Pages/Home.js
+++ project01/src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import TopicBox from '../components/TopicBox'
 import Content from '../components/Content'
+import {Link} from 'react-router-dom'
 
 export default function Home() {
   return (
     <div>
       <h1>Homepage</h1>
-      
+      <Link to="/About">Go to the about page</Link>
+      <Link to="/contact">Go to the contact page</Link>
     </div>
   )
 }
