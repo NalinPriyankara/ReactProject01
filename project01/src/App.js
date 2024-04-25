aa8f57a313136fa2c50660bd344e146d9f6c52b7
NalinPriyankara <112267206+NalinPriyankara@users.noreply.github.com>
2024-04-25 20:16:31 +0530

    add components

--- project01/src/App.js
+++ project01/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Part1 from './components/Part1';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is my first react project</h1>
-      <Part1/>
-      <Part1/>
+      <Home/>
     </div>
   );
 }
